5fb8b1d7e675e2a3ee7841a47f966bbce2a9b853
Jitanshu Shaw <jitanshu.shaw1@gmail.com>
2024-09-01 20:40:09 +0530

    styled poll completion page

--- src/pages/PollCompletionPage.jsx
+++ src/pages/PollCompletionPage.jsx
@@ -3,8 +3,8 @@ import "../css/pollCompletionPage.css";
 
 const PollCompletionPage = () => {
   return (
-    <div className="poll-completion-container">
-      <div className="poll-completion-box">
+    <div className="poll-full-screen-wrapper">
+      <div className="poll-completion-container">
         <h2>Thank you for participating in the Poll</h2>
       </div>
     </div>
